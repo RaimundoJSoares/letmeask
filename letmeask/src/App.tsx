81723e860d6376558eff850aa77479a675ebeed1
RaimundoJSoares <raianimes1000@gmail.com>
2022-01-05 17:17:04 -0400

    criando rotas entre paginas

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,10 +1,16 @@
-//import {Home} from "./pages/Home";
+import { BrowserRouter, Routes , Route } from 'react-router-dom' //versao 6.0 do react tem que usar routes em volta de route
 
+import {Home} from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
 function App() {
   return (
-    <NewRoom />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/"  element={<Home/>} />
+      <Route path="/rooms/new" element={<NewRoom/>} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
