ba04e270b78bc864341e25471882a4fdd1e7e748
RaimundoJSoares <raianimes1000@gmail.com>
2022-01-10 18:31:53 -0400

    criando perguntas

--- letmeask/src/components/RoomCode.tsx
+++ letmeask/src/components/RoomCode.tsx
@@ -1,13 +1,21 @@
 import copyImg from  "../assets/images/copy.svg";
 import '../styles/room-code.scss';
 
-export function RoomCode () {
+type RoomCodeProps = {
+  code: string;
+}
+
+export function RoomCode (props: RoomCodeProps) {
+  function copyRoomToClipeBoard(){
+    navigator.clipboard.writeText(props.code);
+  }
+
   return (
-    <button className= "room-code">
+    <button className= "room-code" onClick={copyRoomToClipeBoard}>
       <div>
         <img src={copyImg} alt="Copy room code" />
       </div>
-      <span>Sala #12132143414</span>
+      <span>Sala #{props.code}</span>
     </button>
   )
 }
--- letmeask/src/pages/Room.tsx
+++ letmeask/src/pages/Room.tsx
@@ -1,15 +1,55 @@
+import { FormEvent, useState } from 'react';
+import {useParams} from 'react-router-dom';
+
 import logoImg from "../assets/images/logo.svg";
 import { Button } from '../components/button';
 import { RoomCode } from "../components/RoomCode";
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 import '../styles/room.scss';
 
+type RoomParams = {
+  id: string ;
+}
+
 export function Room(){
+  const {user} = useAuth();
+  const params = useParams<RoomParams>(); //generic, parametro para typagem
+  const [newQuestion, setNewQuestion] = useState ('');
+
+  const roomid = params.id!;
+
+  async function handleSendQuestion(event: FormEvent){
+    event.preventDefault(); //para não recarregar a tela
+
+    if(newQuestion.trim() === '') {
+      return;
+    }
+    if (!user){
+      throw new Error('You must logged in')
+    }
+    
+    const question = {
+      const: newQuestion,
+      author: {
+        name:user.name,
+        avatar: user.avatar
+      },
+      isHighLighted: false, //se está sendo respondida atualmente
+      isAnswered: false,  //se ja foi respondida ou não(começa como falso claro)
+    };
+
+    await database.ref(`rooms/${roomid}/questions`).push(question); // 0000000
+
+    setNewQuestion(''); //vai assumir valor vazio depois que enviar a pergunta
+  }
+
   return(
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
-          <RoomCode />
+          <RoomCode code={roomid} />
         </div>
       </header>
 
@@ -18,13 +58,15 @@ export function Room(){
           <h1>Sala react</h1>
           <span>4 perguntas</span>
         </div>
-        <form>
+        <form onSubmit={handleSendQuestion}>
           <textarea
             placeholder="O que você quer perguntar?"
+            onChange={event => setNewQuestion(event.target.value)} //valor do imput conforme é digitado
+            value={newQuestion}
           />
           <div className="form-footer">
             <span> Para enviar uma pergunta, <button>faça seu login</button> </span>
-            <Button type="submit">Enviar pergunta</Button>
+            <Button type="submit" disabled={!user}>Enviar pergunta</Button>
           </div>
         </form>
       </main>
